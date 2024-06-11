38c4c3eca961917d5742aff1226a608c6b1a4c75
Marius Debussche <marius.debussche@gmail.com>
2024-06-11 14:49:27 +0200

    Add game layer

--- keyboards/beekeeb/piantor/keymaps/feldspath/keymap.c
+++ keyboards/beekeeb/piantor/keymaps/feldspath/keymap.c
@@ -10,6 +10,8 @@ enum Layer {
     _LOWER = 1,
     _RAISE = 2,
     _ADJUST = 3,
+    _GAME = 4,
+    _GAMELOWER = 5,
 };
 
 enum CustomKeycodes {
@@ -83,10 +85,24 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [_ADJUST] = LAYOUT_split_3x6_3(
         _______, KC_F1,    KC_F2,     KC_F3,       KC_F4,      KC_F5,                         KC_F6,            KC_F7,      KC_F8,      KC_F9,      KC_F10,    _______,
-        _______, KC_F11,   _______,   _______,     _______,    _______,                       QK_BOOTLOADER,    _______,    _______,    _______,    KC_F12,    _______,
+        _______, KC_F11,   _______,   DF(_GAME),   _______,    _______,                       QK_BOOTLOADER,    _______,    _______,    _______,    KC_F12,    _______,
         _______, _______,  _______,   _______,     _______,    _______,                       _______,          _______,    _______,    _______,    _______,   _______,
                                                    _______,    _______,  _______,             _______,          _______,    _______
-    )
+    ),
+
+    [_GAME] = LAYOUT_split_3x6_3( 
+        KC_ESC,  KC_Q,   KC_W,    KC_E,    KC_R,       KC_T,                               KC_Y,       KC_U,         KC_I,         KC_O,         KC_P,         KC_DEL,
+        KC_TAB,  KC_A,   KC_S,    KC_D,    KC_F,       KC_G,                               KC_H,       KC_J,         KC_K,         KC_L,         KC_M,         KC_ENT,
+        KC_LCTL, KC_Z,   KC_X,    KC_C,    KC_V,       KC_B,                               KC_N,       KC_SCLN,      KC_COMM,      KC_DOT,       KC_SLSH,      DF(_ALPHA),
+                                           MO(_GAMELOWER), KC_SPC, KC_EQL,          _______,   _______,    _______
+    ),
+
+    [_GAMELOWER] = LAYOUT_split_3x6_3(
+        _______, KC_1,      KC_2,      KC_3,      KC_4,      KC_5,                       _______, _______, _______, _______, _______, _______,
+        _______, _______,   _______,   _______,   _______,   _______,                    _______, _______, _______, _______, _______, _______,
+        _______, _______,   _______,   _______,   _______,   _______,                    _______, _______, _______, _______, _______, _______,
+                                                  _______,   _______, _______, _______,  _______, _______
+    ),
 };
 
 layer_state_t layer_state_set_user(layer_state_t state) { return update_tri_layer_state(state, _LOWER, _RAISE, _ADJUST); }
